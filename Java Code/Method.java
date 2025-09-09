5b1ce230494e88105f443e97910060ca2b9587ea
kumarsarish32 <kumarsarish32@gmail.com>
2025-09-09 19:25:51 +0530

    Create Method.java

---
+++ Java Code/Method.java
@@ -0,0 +1,10 @@
+public class MethodExample {
+    public void display() {
+        System.out.println("This is a method");
+    }
+
+    public static void main(String[] args) {
+        MethodExample obj = new MethodExample();
+        obj.display();
+    }
+}
