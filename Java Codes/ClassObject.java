218bc9ee789822d828b5a5974658cc4c91bff87f
kumarsarish32 <kumarsarish32@gmail.com>
2025-10-07 21:40:28 +0530

    Delete Java Codes directory

--- Java Codes/ClassObject.java
+++
@@ -1,8 +0,0 @@
-public class ClassObjectExample {
-    String name = "Java";
-
-    public static void main(String[] args) {
-        ClassObjectExample obj = new ClassObjectExample();
-        System.out.println(obj.name);
-    }
-}
